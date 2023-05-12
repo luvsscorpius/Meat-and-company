d014a2a363c07b3e7db2a868ad67b10c66018a07
and <andersonvittor1801@gmail.com>
2023-05-12 18:35:18 -0300

    Quarto commit

---
+++ js/produtos.js
@@ -0,0 +1,5 @@
+let produtosJson = [
+    { id: 1, type: 'carne', name: 'Big Mac', img: 'img/MC/carne-bovina/big-mac.png', kc: '499 kcal', price: 20.00, description: 'Dois hambúrgueres (100% carne bovina), alface americana, queijo sabor cheddar, molho especial, cebola, picles e pão com gergelim.' },
+
+    { id: 2, type: 'frango', name: 'Chicken Jr.', img: 'img/MC/frango/chickenJr.png', kc: '342.5 kcal', price: 25.00, description: 'Frango empanado, maionese, alface americana e pão com gergelim.' }
+]
--- js/script.js
+++ js/script.js
@@ -12,3 +12,74 @@ mode.addEventListener('click', (event) => {
         main.classList.remove('dark')
     }
 })
+
+// Funcoes auxiliares 
+const seleciona = (elemento) => document.querySelector(elemento)
+
+const carneBovina = document.querySelector('.item-carne')
+
+carneBovina.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('cliquei na area carne')
+    const carneArea = seleciona('.carne-bovina-area')
+    if (carneArea.style.display = 'none') {
+        seleciona('.carne-bovina-area').style.display = 'grid'
+        seleciona('.frango-area').style.display = 'none'
+        seleciona('.acompanhamentos-area').style.display = 'none'
+        seleciona('.bebidas-frias-area').style.display = 'none'
+    }
+})
+
+const frango = seleciona('.item-frango')
+
+frango.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('Cliquei na area frango')
+    const frangoArea = seleciona('.frango-area')
+    if (frangoArea.style.display = 'none') {
+        seleciona('.frango-area').style.display = 'grid'
+        seleciona('.carne-bovina-area').style.display = 'none'
+        seleciona('.acompanhamentos-area').style.display = 'none'
+        seleciona('.bebidas-frias-area').style.display = 'none'
+    }
+})
+
+const acompanhamentos = seleciona('.item-acompanhamentos')
+
+acompanhamentos.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('Cliquei na area acompanhamentos')
+    const acompanhamentosArea = seleciona('.acompanhamentos-area')
+    if (acompanhamentosArea.style.display = 'none') {
+        seleciona('.acompanhamentos-area').style.display = 'grid'
+        seleciona('.frango-area').style.display = 'none'
+        seleciona('.carne-bovina-area').style.display = 'none'
+        seleciona('.bebidas-frias-area').style.display = 'none'
+    }
+})
+
+const bebidasFrias = seleciona('.item-bebidas-frias')
+
+bebidasFrias.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log('Cliquei na area bebidas frias')
+    const bebidasFriasArea = seleciona('.bebidas-frias-area')
+    if (bebidasFriasArea.style.display = 'none') {
+        seleciona('.acompanhamentos-area').style.display = 'none'
+        seleciona('.frango-area').style.display = 'none'
+        seleciona('.carne-bovina-area').style.display = 'none'
+        seleciona('.bebidas-frias-area').style.display = 'grid'
+    }
+})
+
+//  Mapear os produtos
+
+produtosJson.map((item, index) => {
+    let carneBovinaItem = seleciona('.models .burger-item').cloneNode(true) //clona os elementos
+    let frangoItem = seleciona('.models .burger-item').cloneNode(true) //clona os elementos
+
+    if (item.type == "carne") {
+        // agora que clonamos precisamos colocar na main
+        seleciona('.carne-bovina-area').append(carneBovinaItem)
+    }
+})
