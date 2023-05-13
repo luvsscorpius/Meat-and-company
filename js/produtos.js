224dce0422ff4c0cbbf3c784804755dec54429c1
and <andersonvittor1801@gmail.com>
2023-05-12 21:32:45 -0300

    Quinto commit

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,9 @@
 let produtosJson = [
     { id: 1, type: 'carne', name: 'Big Mac', img: 'img/MC/carne-bovina/big-mac.png', kc: '499 kcal', price: 20.00, description: 'Dois hambúrgueres (100% carne bovina), alface americana, queijo sabor cheddar, molho especial, cebola, picles e pão com gergelim.' },
 
-    { id: 2, type: 'frango', name: 'Chicken Jr.', img: 'img/MC/frango/chickenJr.png', kc: '342.5 kcal', price: 25.00, description: 'Frango empanado, maionese, alface americana e pão com gergelim.' }
+    { id: 2, type: 'frango', name: 'Chicken Jr.', img: 'img/MC/frango/chickenJr.png', kc: '342.5 kcal', price: 25.00, description: 'Frango empanado, maionese, alface americana e pão com gergelim.' },
+
+    { id: 3, type: 'acompanhamentos', name: 'McFritas Cheddar Bacon', img: 'img/MC/acompanhamentos/mcFritas-cheddar-bacon.png', kc: '525 kcal', price: 15.00, description: 'A batata frita mais famosa do mundo, agora com melt sabor cheddar e bacon crispy. Não dá para resistir, experimente!' },
+
+    { id: 4, type: 'bebidas-frias', name: 'Coca Cola 300mL', img: 'img/MC/bebidas-frias/coca-cola-300ml.png', kc: '129 kcal', price: 12.00, description: 'Refrescante e geladinho. Uma bebida assim refresca a vida. Você pode escolher entre Coca-Cola, Coca-Cola Zero, Fanta Guaraná e Fanta Laranja.' },
 ]
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ mode.addEventListener('click', (event) => {
     if (mode.src.match('moon')) {
         mode.src = '../img/sun.png'
         main.classList.add('dark')
+        burgerItem.classList.add('dark')
     } else {
         mode.src = '../img/moon.png'
         main.classList.remove('dark')
@@ -21,12 +22,12 @@ const carneBovina = document.querySelector('.item-carne')
 carneBovina.addEventListener('click', (event) => {
     event.preventDefault()
     console.log('cliquei na area carne')
-    const carneArea = seleciona('.carne-bovina-area')
+    const carneArea = seleciona('#carne-bovina-area')
     if (carneArea.style.display = 'none') {
-        seleciona('.carne-bovina-area').style.display = 'grid'
-        seleciona('.frango-area').style.display = 'none'
-        seleciona('.acompanhamentos-area').style.display = 'none'
-        seleciona('.bebidas-frias-area').style.display = 'none'
+        seleciona('#carne-bovina-area').style.display = 'grid'
+        seleciona('#frango-area').style.display = 'none'
+        seleciona('#acompanhamentos-area').style.display = 'none'
+        seleciona('#bebidas-frias-area').style.display = 'none'
     }
 })
 
@@ -35,12 +36,12 @@ const frango = seleciona('.item-frango')
 frango.addEventListener('click', (event) => {
     event.preventDefault()
     console.log('Cliquei na area frango')
-    const frangoArea = seleciona('.frango-area')
+    const frangoArea = seleciona('#frango-area')
     if (frangoArea.style.display = 'none') {
-        seleciona('.frango-area').style.display = 'grid'
-        seleciona('.carne-bovina-area').style.display = 'none'
-        seleciona('.acompanhamentos-area').style.display = 'none'
-        seleciona('.bebidas-frias-area').style.display = 'none'
+        seleciona('#frango-area').style.display = 'grid'
+        seleciona('#carne-bovina-area').style.display = 'none'
+        seleciona('#acompanhamentos-area').style.display = 'none'
+        seleciona('#bebidas-frias-area').style.display = 'none'
     }
 })
 
@@ -49,12 +50,12 @@ const acompanhamentos = seleciona('.item-acompanhamentos')
 acompanhamentos.addEventListener('click', (event) => {
     event.preventDefault()
     console.log('Cliquei na area acompanhamentos')
-    const acompanhamentosArea = seleciona('.acompanhamentos-area')
+    const acompanhamentosArea = seleciona('#acompanhamentos-area')
     if (acompanhamentosArea.style.display = 'none') {
-        seleciona('.acompanhamentos-area').style.display = 'grid'
-        seleciona('.frango-area').style.display = 'none'
-        seleciona('.carne-bovina-area').style.display = 'none'
-        seleciona('.bebidas-frias-area').style.display = 'none'
+        seleciona('#acompanhamentos-area').style.display = 'grid'
+        seleciona('#frango-area').style.display = 'none'
+        seleciona('#carne-bovina-area').style.display = 'none'
+        seleciona('#bebidas-frias-area').style.display = 'none'
     }
 })
 
@@ -63,23 +64,52 @@ const bebidasFrias = seleciona('.item-bebidas-frias')
 bebidasFrias.addEventListener('click', (event) => {
     event.preventDefault()
     console.log('Cliquei na area bebidas frias')
-    const bebidasFriasArea = seleciona('.bebidas-frias-area')
+    const bebidasFriasArea = seleciona('#bebidas-frias-area')
     if (bebidasFriasArea.style.display = 'none') {
-        seleciona('.acompanhamentos-area').style.display = 'none'
-        seleciona('.frango-area').style.display = 'none'
-        seleciona('.carne-bovina-area').style.display = 'none'
-        seleciona('.bebidas-frias-area').style.display = 'grid'
+        seleciona('#acompanhamentos-area').style.display = 'none'
+        seleciona('#frango-area').style.display = 'none'
+        seleciona('#carne-bovina-area').style.display = 'none'
+        seleciona('#bebidas-frias-area').style.display = 'grid'
     }
 })
+
+// Função para preencher os dados dos produtos na main
+const preencherDadosDoItem = (itemElement, item, index) => {
+    itemElement.querySelector('.burger-item--id').value = index
+    itemElement.querySelector('.burger-item--img img').src = item.img
+    itemElement.querySelector('.burger-item--name').innerHTML = item.name
+    itemElement.querySelector('.burger-item--price').innerHTML = item.price
+}
 
 //  Mapear os produtos
 
 produtosJson.map((item, index) => {
     let carneBovinaItem = seleciona('.models .burger-item').cloneNode(true) //clona os elementos
-    let frangoItem = seleciona('.models .burger-item').cloneNode(true) //clona os elementos
+    let frangoItem = seleciona('.models .burger-item').cloneNode(true)
+    let acompanhamentosItem = seleciona('.models .burger-item').cloneNode(true)
+    let bebidasFriasItem = seleciona('.models .burger-item').cloneNode(true)
+
 
     if (item.type == "carne") {
         // agora que clonamos precisamos colocar na main
-        seleciona('.carne-bovina-area').append(carneBovinaItem)
+        seleciona('#carne-bovina-area').append(carneBovinaItem)
+
+        // agora precisamos preencher os dados 
+        preencherDadosDoItem(carneBovinaItem, item, index)
+    }
+
+    if (item.type == "frango") {
+        seleciona('#frango-area').append(frangoItem)
+        preencherDadosDoItem(frangoItem, item, index)
+    }
+
+    if (item.type == "acompanhamentos") {
+        seleciona('#acompanhamentos-area').append(acompanhamentosItem)
+        preencherDadosDoItem(acompanhamentosItem, item, index)
+    }
+
+    if (item.type == "bebidas-frias") {
+        seleciona('#bebidas-frias-area').append(bebidasFriasItem)
+        preencherDadosDoItem(bebidasFriasItem, item, index)
     }
 })
